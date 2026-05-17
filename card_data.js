14d649fced2687a49ea03c65af6c4a7217e77958
Mireille <virtuekyoko@gmail.com>
2026-05-17 19:12:59 +0800

    Delete card_data.js

--- card_data.js
+++
@@ -1,9 +0,0 @@
-// card_data.js 內容只要這一段就好
-const MY_CARD_DATA = [
-{"id":0,"name":"現金(早餐)","cls":"cash","s":1,"d":31,"type":"cash","details":[],"paid":false},
-{"id":1,"name":"中信","cls":"ctbc","s":22,"d":8,"details":[{"date":"03/29","note":"登山包","amt":558}],"paid":false},
-{"id":2,"name":"台新","cls":"ts","s":6,"d":21,"details":[],"paid":false},
-{"id":3,"name":"玉山","cls":"esun","s":13,"d":28,"details":[{"date":"04/15","note":"必勝客","amt":985},{"date":"04/15","note":"家樂福","amt":1199},{"date":"04/15","note":"家樂福-十八庄","amt":8055},{"date":"04/15","note":"可不可","amt":269},{"date":"04/16","note":"食見生活-餐盒","amt":2074},{"date":"04/13","note":"波奇波奇","amt":179}],"paid":false},
-{"id":4,"name":"合庫","cls":"tcb","s":1,"d":17,"details":[],"paid":false},
-{"id":5,"name":"星展","cls":"dbs","s":8,"d":27,"details":[],"paid":false}
-];
